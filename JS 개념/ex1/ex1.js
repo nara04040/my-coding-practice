2ce23d9a783b9bb5f5c1ba31091ab3f4570b474f
nara04040 <83911617+nara04040@users.noreply.github.com>
2021-12-08 15:20:22 +0900

    21.12.08
    cafe

---
+++ JS 개념/ex1/ex1.js
@@ -0,0 +1,5 @@
+const age = parseInt(prompt("how old are you?"));
+
+console.log(age);
+
+//parseInt = string 을 number처럼 type을 변경할때 쓰임
---
+++ 바닐라js 크롬/ex1/ex1.js
@@ -0,0 +1,5 @@
+// recap : + - * / and console.log(a, b)
+
+const age = promt("How old are you"); // promt = show whindow
+
+console.log(age);
